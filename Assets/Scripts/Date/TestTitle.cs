fa3314d983fe738cb2f6a8e7d762c99ae1b9f776
date <roxas03200320@gmail.com>
2018-08-31 11:15:19 +0900

    シーン管理
    シーン遷移とCanvasImageでのFade

---
+++ Assets/Scripts/Date/TestTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTitle : MonoBehaviour {
+
+	public void OnButton1_Click()
+    {
+        SceneController.GetInstance.ChangeScene(_sceneName: "StageSelectScene");
+    }
+
+    public void OnButton2_Click()
+    {
+        SceneController.GetInstance.ChangeScene(_sceneName: "GameScene");
+    }
+
+    public void OnButton3_Click()
+    {
+        SceneController.GetInstance.ChangeScene(_sceneName: "TitleScene");
+    }
+}
