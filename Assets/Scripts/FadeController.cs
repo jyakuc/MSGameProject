c95a838e5ba962623ecf5d38f74d4f969651195a
date <roxas03200320@gmail.com>
2018-08-31 11:53:02 +0900

    Revert "Revert "シーン管理""
    This reverts commit 2f1c4b13f14ffa7df1ac791cdb5d040e953447cc.

---
+++ Assets/Scripts/FadeController.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// CanvasGroupがアタッチされる
+[RequireComponent(typeof(CanvasGroup))]
+
+public class FadeController : MonoBehaviour {
+
+    // フェード状態
+    private enum FadeState
+    {
+        FadeIn,
+        FadeOut,
+        None
+    }
+    private FadeState m_fadeState = FadeState.None;
+
+    // フェード中かどうか
+    public bool IsFade
+    {
+        get {
+
+            return m_fadeState != FadeState.None;
+        }
+    }
+
+    // フェード用キャンバス
+    private CanvasGroup m_canvasGroup;
+    private CanvasGroup CanvasGroup
+    {
+        get {
+            if(m_canvasGroup == null)
+            {
+                if((m_canvasGroup = GetComponent<CanvasGroup>()) == null)
+                { 
+                    m_canvasGroup = gameObject.AddComponent<CanvasGroup>();
+                }
+            }
+            return m_canvasGroup;
+        }
+    }
+
+    // フェードのアルファ値
+    public float Alpha
+    {
+        get { return CanvasGroup.alpha; }
+        set { CanvasGroup.alpha = value; }
+    }
+
+    // フェード時間
+    [SerializeField]
+    private float m_duration;
+    public float Duration
+    {
+        get { return m_duration; }
+    }
+
+    // タイムスケールを無視するかどうか
+    [SerializeField]
+    private bool m_ignoreTimeScale = true;
+
+    // フェード終了後のコールバック
+    private event Action m_onFinished = null;
+
+    
+	// Update is called once per frame
+	void Update () {
+        if (!IsFade) return;
+
+        float fadeSpeed = 1.0f / m_duration;
+        if (m_ignoreTimeScale)
+        {
+            fadeSpeed *= Time.unscaledDeltaTime;
+        }
+        else
+        {
+            fadeSpeed *= Time.deltaTime;
+        }
+
+        if(m_fadeState == FadeState.FadeIn)
+        {
+            Alpha += fadeSpeed * 1.0f;
+        }
+        else
+        {
+            Alpha += fadeSpeed * -1.0f;
+        }
+
+        // Fade中
+        if (Alpha > 0 && Alpha < 1) return;
+
+        m_fadeState = FadeState.None;
+        this.enabled = false;
+
+        if(m_onFinished != null)
+        {
+            m_onFinished();
+        }
+	}
+
+    /// <summary>
+    /// Objectのフェード開始
+    /// </summary>
+    /// <param name="_target"></param>
+    /// <param name="_isFadeOut"></param>
+    /// <param name="_duration"></param>
+    public static void Begin(GameObject _target,bool _isFadeOut,float _duration)
+    {
+        FadeController fadeController = _target.GetComponent<FadeController>();
+        if(fadeController == null)
+        {
+            fadeController = _target.AddComponent<FadeController>();
+        }
+        fadeController.enabled = true;
+
+        fadeController.Play(_isFadeOut,_duration);
+
+    }
+
+    /// <summary>
+    /// 実際にフェードを開始
+    /// </summary>
+    /// <param name="_isFadeOut"></param>
+    /// <param name="_duration"></param>
+    /// <param name="_ignoreTimeScale"></param>
+    /// <param name="_onFinished"></param>
+    public void Play(bool _isFadeOut,float _duration,bool _ignoreTimeScale = true, Action _onFinished = null)
+    {
+        this.enabled = true;
+
+        m_ignoreTimeScale = _ignoreTimeScale;
+        m_onFinished = _onFinished;
+
+        if (_isFadeOut)
+        {
+            Alpha = 1;
+            m_fadeState = FadeState.FadeOut;
+        }
+        else
+        {
+            Alpha = 0;
+            m_fadeState = FadeState.FadeIn;
+
+            m_duration = _duration;
+        }
+
+    }
+
+    /// <summary>
+    /// フェード停止
+    /// </summary>
+    public void Stop()
+    {
+        m_fadeState = FadeState.None;
+        this.enabled = false;
+    }
+
+    public void FadeIn()
+    {
+
+    }
+
+    public void FadeOut()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : SingletonMonoBehaviour<SceneController> {
+
+    [SerializeField]
+    private FadeController m_fader = null;
+    public bool IsFadeing
+    {
+        get{ return m_fader.IsFade;  }
+    }
+
+    public const float FADE_TIME = 0.5f;
+    private float m_fadeTime = FADE_TIME;
+
+
+    private string m_oldSceneName = "";
+    public string OldSceneName
+    {
+        get { return m_oldSceneName; }
+    }
+
+    private string m_currentSceneName = "";
+    public string CurrentSceneName
+    {
+        get { return m_currentSceneName; }
+    }
+
+    private string m_nextSceneName = "";
+    public string NextSceneName
+    {
+        get { return m_nextSceneName; }
+    }
+
+    // フェード後のイベント
+    public event Action onFadeOutFinished = null;
+    public event Action onFadeInFinished = null;
+
+
+    /// <summary>
+    /// 初期化メソッド(Awake時か　それ以前のアクセス　の　1回でしか呼ばれない)
+    /// </summary>
+    protected override void Init()
+    {
+        base.Init();
+
+        if(m_fader == null)
+        {
+            Reset();
+        }
+
+        // 最初のシーン名を設定
+        m_currentSceneName = SceneManager.GetSceneAt(0).name;
+
+        // 破棄されないようにする
+        DontDestroyOnLoad(gameObject);
+
+        m_fader.gameObject.SetActive(false);
+    }
+
+    // コンポーネント追加時に実行
+    private void Reset()
+    {
+        gameObject.name = "SceneController";
+
+        // フェード用のキャンバス作成
+        GameObject fadeCavas = new GameObject("FadeCanvas");
+        fadeCavas.transform.SetParent(transform);
+        fadeCavas.SetActive(false);
+
+        Canvas canvas = fadeCavas.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.sortingOrder = 999;
+
+        fadeCavas.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        fadeCavas.AddComponent<GraphicRaycaster>();
+        m_fader = fadeCavas.AddComponent<FadeController>();
+        m_fader.Alpha = 0;
+
+        //CreateFadeImage(fadeCavas , new Vector2(2000,2000));
+    }
+
+    /// <summary>
+    /// フェード用イメージ作成
+    /// </summary>
+    /// <param name="_obj">親にするCanvas</param>
+    /// <param name="_size">イメージサイズ</param>
+    protected void CreateFadeImage(GameObject _obj,Vector2 _size)
+    {
+        GameObject imageObj = new GameObject("Image");
+        imageObj.transform.SetParent(_obj.transform, false);
+        imageObj.AddComponent<Image>().color = Color.black;
+        imageObj.GetComponent<RectTransform>().sizeDelta = _size;
+    }
+
+    /// <summary>
+    /// シーン変更
+    /// </summary>
+    /// <param name="_sceneName">遷移するシーンの名前</param>
+    /// <param name="_fadaTime">フェードする時間</param>
+    public void ChangeScene(string _sceneName , float _fadaTime = FADE_TIME)
+    {
+        if (IsFadeing)
+        {
+            Debug.LogError("フェード中にシーンを変更しています");
+            return;
+        }
+
+        m_nextSceneName = _sceneName;
+        m_fadeTime = _fadaTime;
+
+        // フェードイン
+        m_fader.gameObject.SetActive(true);
+        m_fader.Play(_isFadeOut: false, _duration: m_fadeTime , _onFinished:OnFadeOutFinish);
+
+    }
+
+    private void OnFadeOutFinish()
+    {
+        if (onFadeOutFinished != null)
+        {
+            onFadeOutFinished();
+        }
+
+        SceneManager.LoadScene(m_nextSceneName);
+
+        m_oldSceneName = m_currentSceneName;
+        m_currentSceneName = m_nextSceneName;
+
+        //フェードアウト
+        m_fader.gameObject.SetActive(true);
+        m_fader.Alpha = 1;
+        m_fader.Play(_isFadeOut: true, _duration: m_fadeTime, _onFinished: OnFadeInFinish);
+    }
+
+    private void OnFadeInFinish()
+    {
+        m_fader.gameObject.SetActive(false);
+        if(onFadeInFinished != null)
+        {
+            onFadeInFinished();
+        }
+
+    }
+    
+}
