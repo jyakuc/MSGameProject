a6e29fd1caec937592ea32cef06777dc76e172b1
miyahara <taru020202@gmail.com>
2018-09-03 14:28:04 +0900

    StageCreate
    Prefab toka ippai tukurimasita

---
+++ Assets/Scripts/Miyahara/StageCreate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour {
+
+    // プレハブを取得
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        
+        // プレハブからインスタンスを生成
+        Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
