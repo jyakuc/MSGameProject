4344664a89b6085d5b7c062c7ea65ee97c7784fa
date <roxas03200320@gmail.com>
2018-09-03 12:01:54 +0900

    テストモデル追加

---
+++ Assets/Prefabs/Date/Seed/Seedparticle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seedparticle : MonoBehaviour {
+    ParticleSystem ParticleData;
+    float StartTime = 0;
+    Transform TopPos;
+    Transform Pos;
+    GameObject hiyama;
+    public AnimatorStateInfo aniinfo;
+    public float anitime;
+    bool One = false;
+	// Use this for initialization
+	void Start () {
+        ParticleData = GetComponent<ParticleSystem>();
+        hiyama = GameObject.Find("hiiyama");
+        TopPos = GameObject.Find("top").GetComponent<Transform>();
+        Pos = GetComponent<Transform>();
+        Pos = TopPos;
+        aniinfo = hiyama.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Pos = TopPos;
+        aniinfo = hiyama.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        anitime = aniinfo.normalizedTime;
+        //Debug.Log(anitime);
+        if (anitime>=0.75)
+        {
+            if(!One)
+            {
+                ParticleData.Play();
+                One = true;
+            }
+
+
+        }
+        else
+        {
+            One = false;
+        }
+	}
+}
