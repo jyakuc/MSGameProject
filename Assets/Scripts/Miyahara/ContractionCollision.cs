1a6aae0ae15013890bfdd31b804f427d9de0fa98
miyahara <taru020202@gmail.com>
2018-09-10 19:30:42 +0900

    NomalStageUpdate
    ステージに当たり判定とグラグラとステージ落下を入れました。

---
+++ Assets/Scripts/Miyahara/ContractionCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContractionCollision : MonoBehaviour {
+
+    private float x, y, z;  //収縮オブジェクトのXYZ取得
+
+
+
+	// Use this for initialization
+	void Start () {
+        x = 40.0f;
+        y = 2.0f;
+        z = 40.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        x += -0.05f;
+        //y += -0.01f;
+        z += -0.05f;
+
+        if (x >= 2 && y >= 2 && z >= 2)
+        {
+            this.transform.localScale = new Vector3(x, 2, z);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/Miyahara/StageCollision.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCollision : MonoBehaviour {
+
+    public GameObject Collsion;
+
+    private Rigidbody Rb;
+
+    private bool WobblyFlg;
+
+    private int MaxWobbly = 10;
+
+    private float Angle;
+    private float Speed = 5.0f;
+    private float Maxangle = 30.0f;
+    private float Minangle = -30.0f;
+    private int Count = 0;
+    
+
+    public enum Direction
+    {
+        Right,
+        Left
+    }
+
+    public Direction Status;
+
+    void Start()
+    {
+        Rb = this.GetComponent<Rigidbody>();
+        Rb.useGravity = false;
+        WobblyFlg = false;
+        
+    }
+
+    void OnTriggerExit(Collider Collsion)
+    {
+        WobblyFlg = true;
+        Debug.Log("hanareta");
+    }
+
+    void Update()
+    {
+        if (WobblyFlg)
+        {
+            switch (Status) {
+                case Direction.Left:
+                    Angle += Speed;
+                    transform.eulerAngles = new Vector3(0, 0, Angle);
+                    if (Angle >= Maxangle)
+                    {
+                        Debug.Log("left");
+                        Status = Direction.Right;
+                        Count += 1;
+                    }
+                    break;
+                case Direction.Right:
+                    Angle -= Speed;
+                    transform.eulerAngles = new Vector3(0, 0, Angle);
+                    if (Angle <= Minangle)
+                    {
+                        Debug.Log("right");
+                        Status = Direction.Left;
+                        Count += 1;
+                    }
+                    break;
+            }
+            if (Count >= MaxWobbly)
+            {
+                Rb.useGravity = true;
+                Rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            }
+        }
+        
+    }
+}
