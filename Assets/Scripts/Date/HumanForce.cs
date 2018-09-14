03388350034b0b7328d7bec89723245ce8ee0c0c
date <roxas03200320@gmail.com>
2018-09-14 09:15:55 +0900

    モデル追加

---
+++ Assets/Scripts/Date/HumanForce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanForce : MonoBehaviour {
+    public GameObject humanModel;
+    public GameObject headEnd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // if (Input.GetKeyDown(KeyCode.Space))
+       // {
+       //     headEnd.GetComponent<Rigidbody>().AddForce(0, 0, -5.0f);
+       // }	
+	}
+}
