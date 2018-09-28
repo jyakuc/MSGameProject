017529df9c2bc3f4166b693d554a64405749f147
date <roxas03200320@gmail.com>
2018-09-28 09:59:10 +0900

    モデルのボーンにBoxCollide付与

--- Assets/Scripts/Date/HumanForce.cs
+++ Assets/Scripts/Date/HumanForce.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HumanForce : MonoBehaviour {
     public GameObject humanModel;
+    public Animator animator;
     public GameObject headEnd;
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,10 @@ public class HumanForce : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       // if (Input.GetKeyDown(KeyCode.Space))
-       // {
-       //     headEnd.GetComponent<Rigidbody>().AddForce(0, 0, -5.0f);
-       // }	
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+       
+             headEnd.GetComponent<Rigidbody>().AddForce(0, 0, -5.0f);
+        }	
 	}
 }
---
+++ Assets/Scripts/Date/IKController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKController : MonoBehaviour {
+
+    protected Animator animator;
+
+    public bool ikActive = false;
+    public Transform rightHandObj = null;
+    public Transform leftHandObj = null;
+    public Transform rightFootObj = null;
+    public Transform leftFootObj = null;
+    public Transform lookObj = null;
+    
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!animator) return;
+
+        if (ikActive)
+        {
+            // 指定されている場合、視線のターゲット位置を設定します
+            if(lookObj != null)
+            {
+                animator.SetLookAtWeight(1);
+                animator.SetLookAtPosition(lookObj.position);
+            }
+            // 指定されている場合、右手のターゲット位置と回転を設定します
+            if(rightHandObj != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                
+            }
+            if(leftHandObj != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+                animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+            }
+            if(rightFootObj != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+                animator.SetIKPosition(AvatarIKGoal.RightFoot, rightFootObj.position);
+                animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootObj.rotation);
+            }
+            if(leftFootObj != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
+                animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootObj.position);
+                animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootObj.rotation);
+            }
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+            animator.SetLookAtWeight(0);
+        }
+    }
+}
