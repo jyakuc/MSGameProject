668bc0baae8d09a4372aa988568ec9bbf7c93d20
date <roxas03200320@gmail.com>
2018-10-01 14:24:15 +0900

    ゲーム制御スクリプト編集中

---
+++ Assets/Scripts/Date/EStageIndex.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EStageIndex
+{
+    Stage_1,
+    Stage_2,
+    Stage_3,
+    Stage_4
+}
---
+++ Assets/Scripts/Date/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int m_players;
+    public int Players
+    {
+        get { return m_players; }
+    }
+
+    public PlayerController[] m_playerCtrllers;
+    public StageCreate m_stageCreate;
+    public GameObject m_playerModel;
+
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+       // m_stageCreate.Create(EStageIndex.Stage_1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayerCreate(EStageIndex _stage)
+    {
+
+        for (int i = 0; i < m_players; ++i) {
+            m_playerCtrllers[i] = Instantiate<GameObject>(m_playerModel).GetComponent<PlayerController>();
+        }
+    }
+}
