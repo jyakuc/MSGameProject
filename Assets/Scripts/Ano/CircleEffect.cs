fc4a69132a2f2e87af3344947664ab5798e8d105
Anottidesu <Anottidesu@users.noreply.github.com>
2018-10-11 23:27:15 +0900

    今までの作業分(EffectとResultを追加しました)
    AnoSceneにeffect
    ResultにResultの作業分追加

---
+++ Assets/Scripts/Ano/CircleEffect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleEffect : MonoBehaviour {
+    //サイズ変更適用Transform
+    private Transform CircleTrans;
+    //色変更用
+    public Color ColorData;
+    //サイズ用変数
+    [Range(0, 10)]
+    public float CircleSize = 0;
+    //サイズ減少用変数
+    [Range(0, 10)]
+    public float DecreaseSpeed = 0;
+    //サイズ限界値変数
+    [Range(0, 10)]
+    public float LimitSize = 0;
+    private void Awake()
+    {
+        ParticleSystem.MainModule CircleParticle = GetComponent<ParticleSystem>().main;
+        CircleTrans = GetComponent<Transform>();
+        CircleParticle.startColor = ColorData;
+        CircleTrans.localScale = new Vector3(CircleSize, CircleSize, CircleSize);
+        
+    }
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        CircleSize -= DecreaseSpeed;
+        if(CircleSize<= LimitSize)
+        {
+            CircleSize = LimitSize;
+        }
+        CircleTrans.localScale = new Vector3(CircleSize, CircleSize, 1);
+
+    }
+
+    
+
+}
---
+++ Assets/Scripts/Ano/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Filter : MonoBehaviour {
+    //サイズ変更適用Transform
+    Transform Trans;
+    //サイズ減少用変数
+    [Range(0, 20)]
+    public float Speed = 0;
+    //サイズ限界値変数
+    [Range(0, 500)]
+    public float LimitSize = 0;
+    // Use this for initialization
+    void Start () {
+        Trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Trans.localScale.x<= LimitSize)
+        {
+            Trans.localScale = new Vector3(LimitSize, LimitSize, Trans.localScale.z);
+        }
+        else
+        {
+            Trans.localScale = new Vector3(Trans.localScale.x-Speed, Trans.localScale.y - Speed, Trans.localScale.z);
+
+        }
+    }
+}
