b27b3a05ad71bdace07ff81520b5e4043ee119a5
Anottidesu <Anottidesu@users.noreply.github.com>
2018-10-25 18:29:23 +0900

    モデルの背中にロゴ的なのを入れれるように修正、スコア用の描画処理追加
    Humanのモデル側にMaterial追加しないといけなかったので
    FBX新しく作って今ある新しいHumanのPrefabを作りました
    Playe1P用のPrefabの中のデーターほぼ同じ状態にしましたがどこか抜けてたり変なことあれば教えてください
    場所はPrefabs→Ano→HumanTestにPrefab作りました

---
+++ Assets/Scripts/Ano/Score.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    //スコア格納用
+    public int ScoreNum = 0;
+    //最大文字数
+    [Range(1, 3)]
+    public int Max_Character = 0;
+    //文字に変換用
+    public string ScoreText;
+    private int CheckCount = 0;
+    // Use this for initialization
+    private void Awake()
+    {
+        ScoreText = "   ";
+        GetScoreData(ScoreNum);
+        //ScoreText = ScoreNum.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CheckCharacter();
+
+    }
+    public void GetScoreData(int Num)
+    {
+        ScoreNum = Num;
+        if (ScoreNum > 999)
+        {
+            ScoreText = "999";
+        }
+        else if(ScoreNum>=10)
+        {
+            ScoreText = ScoreNum.ToString();
+            ScoreText = ScoreText.Insert(ScoreText.Length, " ");
+        }
+        else if (ScoreNum>= 0)
+        {
+            ScoreText = ScoreNum.ToString();
+            ScoreText = ScoreText.Insert(ScoreText.Length, "  ");
+
+
+        }
+
+    }
+    public char GetTextPos(int Pos)
+    {
+        //最大文字数より出ていなければその位置の文字を渡す
+        if (Pos < Max_Character)
+        {
+
+            return ScoreText[Pos];
+        }
+        //超えている場合空文字とエラーを返す
+        Debug.Log("Character Count error");
+        return ' ';
+
+    }
+    //文字チェック（半角大文字英数字以外が含まれているとfarceを返す
+    bool CheckCharacter()
+    {
+        CheckCount = 0;
+        //0～9までの判定ループ
+        for (char SearchChar = (char)32; SearchChar < 58; SearchChar++)
+        {
+            if (SearchChar == (char)33)
+            {
+                SearchChar = (char)48;
+            }
+            //名前が格納されているところに全部判定
+            for (int i = 0; i < Max_Character; i++)
+            {
+                if (ScoreText[i] == SearchChar)
+                {
+                    CheckCount += 1;
+                }
+            }
+        }
+        //カウントが最大文字数と同じの場合正常な名前なのでtrue
+        if (CheckCount == Max_Character)
+        {
+            return true;
+        }
+        //それ以外は無効な文字が入っているのでfarce
+        return false;
+    }
+}
---
+++ Assets/Scripts/Ano/ScoreCharacter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreCharacter : MonoBehaviour {
+    //文字の数もし上限する場合変更する
+    public enum CharacterPos { first, second, third};
+    //このスプリプトは何個目の文字か判定する用
+    public CharacterPos C_Pos;
+    //文字格納用
+    public char ScoreText;
+    //プレイヤーネームスプリプト用
+    public Score score;
+    //GUIのImageのマテリアル変更用
+    private Image image;
+    const int Separated = 6;
+    private void Awake()
+    {
+        //Imageを参照
+        image = GetComponent<Image>();
+
+    }
+    // Use this for initialization
+    void Start () {
+        //Scoreスプリプトから自分の文字を取得
+        ScoreText = score.GetTextPos((int)C_Pos);
+        //文字をUVで設定
+        UVTexCharacter();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void UVTexCharacter()
+    {
+        //UV変更用にMaterialを新しく作る
+        Material material = new Material(Shader.Find("GUI/Text Shader"));
+        //UV座標設定
+        material.SetTextureScale("_MainTex", new Vector2(1.0f / 6.0f, 1.0f / 6.0f));
+        material.SetTextureOffset("_MainTex", FontPosGet());
+
+        //ImageのMaterialに新しく作ったMaterialを入れる
+        image.material = material;
+
+
+    }
+    Vector2 FontPosGet()
+    {
+        //文字の0から9までの数値がアルファベットより前なので後扱いにする為に用意
+        int Normalization = ScoreText;
+        //文字の0から9まで判定
+        if ((Normalization >= 48) && (Normalization <= 57))
+        {
+            Normalization += 43;
+        }
+        //空白と空文字の場合
+        if ((Normalization == 0) || (Normalization == 32))
+        {
+            return new Vector2(1, 1);
+        }
+        //今後改良予定今は間に合わせ処理にする
+        Normalization -= 64;
+        int WidthPos = 0;
+        Vector2 CalculatedOffset = Vector2.zero;
+        while (Normalization >= 1)
+        {
+            if ((Normalization <= Separated) && (Normalization >= 1))
+            {
+                if (WidthPos == 0)
+                {
+                    CalculatedOffset = new Vector2(WidthPos, ((float)Separated - (float)Normalization) / (float)Separated);
+                }
+                else
+                {
+                    CalculatedOffset = new Vector2((float)WidthPos / (float)Separated, ((float)Separated - (float)Normalization) / (float)Separated);
+                }
+
+
+            }
+            Normalization -= Separated;
+            WidthPos += 1;
+        }
+        return CalculatedOffset;
+
+
+    }
+}
