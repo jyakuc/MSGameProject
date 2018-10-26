f30e95c409888c92ffed4c6948d00e86689929dd
jyakuc <vbfbap1234@gmail.com>
2018-10-26 09:39:09 +0900

    add,ui,texture,script
    ui関係の追加

---
+++ Assets/Scripts/Ijima/GameUIScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIScripts : MonoBehaviour {
+
+    public GameObject Canvas;
+    public GameObject uiPlayer;
+    public GameObject uiRank;
+
+
+
+    // Use this for initialization
+    void Start () {
+        //PlayerUIの生成
+        GameObject pPlayerUI = (GameObject)Instantiate(uiPlayer);
+        pPlayerUI.transform.SetParent(Canvas.transform, false);
+        //RankingUIの生成
+        GameObject pRankUI = (GameObject)Instantiate(uiRank);
+        pRankUI.transform.SetParent(Canvas.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+}
