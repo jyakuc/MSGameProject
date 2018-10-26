3153b5f340bebf5b1dd14602e8837b5050ad84a6
miyahara <taru020202@gmail.com>
2018-10-26 12:02:00 +0900

    FallCollisionUpdate
    下の当たり判定を追加
    (destroyできない)

--- Assets/Scripts/Miyahara/FallCollision.cs
+++ Assets/Scripts/Miyahara/FallCollision.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FallCollision : MonoBehaviour {
 
+    private const int MaxArray = 6;
+    public int[] Rank = new int[MaxArray];
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,13 +14,18 @@ public class FallCollision : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag != "Player")
+        {
             return;
-        other.gameObject.GetComponent<PlayerController>();
+        }
+            Debug.Log(other.gameObject.name);
+            Rank[Rank.Length]= other.gameObject.GetComponent<PlayerController>().PlayerID;
+            Destroy(other.gameObject);
+            
+
     }
 }
