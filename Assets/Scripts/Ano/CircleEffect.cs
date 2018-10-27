cd2f5bcd2f023a545c9929b1c7e5d143db7540c1
Anottidesu <Anottidesu@users.noreply.github.com>
2018-10-28 02:10:19 +0900

    HitEffectを再生、各モデルのMaterialを個別化、サークルエフェクト修正
    Human_1～6までのPrefab弄ったので競合起きないかチェックお願いします
    各モデルのMaterialはMaterial→Player_番号に置いておきました
    SEの再生は今は現状テスト代わりにSE→MenuのDecisionを使っていますが
    ヒット時のSE追加時にHitEffects.csの171行目のSEを変えます

--- Assets/Scripts/Ano/CircleEffect.cs
+++ Assets/Scripts/Ano/CircleEffect.cs
@@ -5,24 +5,15 @@ using UnityEngine;
 public class CircleEffect : MonoBehaviour {
     //サイズ変更適用Transform
     private Transform CircleTrans;
+    public Transform FilterTrans;
     //色変更用
     public Color ColorData;
-    //サイズ用変数
-    [Range(0, 10)]
-    public float CircleSize = 0;
-    //サイズ減少用変数
-    [Range(0, 10)]
-    public float DecreaseSpeed = 0;
-    //サイズ限界値変数
-    [Range(0, 10)]
-    public float LimitSize = 0;
+
     private void Awake()
     {
         ParticleSystem.MainModule CircleParticle = GetComponent<ParticleSystem>().main;
         CircleTrans = GetComponent<Transform>();
-        CircleParticle.startColor = ColorData;
-        CircleTrans.localScale = new Vector3(CircleSize, CircleSize, CircleSize);
-        
+        CircleParticle.startColor = ColorData;        
     }
     // Use this for initialization
     void Start () {
@@ -31,14 +22,7 @@ public class CircleEffect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-        CircleSize -= DecreaseSpeed;
-        if(CircleSize<= LimitSize)
-        {
-            CircleSize = LimitSize;
-        }
-        CircleTrans.localScale = new Vector3(CircleSize, CircleSize, 1);
-
+        CircleTrans.localScale = FilterTrans.localScale;
     }
 
     
