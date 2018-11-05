b68eaf4f82b83ffed99c787392dbb6d93f52084d
miyahara <taru020202@gmail.com>
2018-11-05 14:28:09 +0900

    ゲームシーンのカメラ遊びです
    カメラ移動→ゲームプレイの流れできました

--- Assets/Scripts/Miyahara/RotateCamera.cs
+++ Assets/Scripts/Miyahara/RotateCamera.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour {
 
     private float num;
+    private float Count;
+    [SerializeField]
+    private GameObject MainCamera;
+    [SerializeField]
+    private GameObject SubCamera;
+    [SerializeField]
+    private GameObject Cannons;
 
+    private float Rate;
 	// Use this for initialization
 	void Start () {
-		
+        num = 2;
+        Rate = 0;
+        Count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Rotate(0, num, 0);
-        num += 1;
+        if (Count < 360)
+        {
+            this.transform.Rotate(0, num, 0);
+            Count += num;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(this.transform.position, MainCamera.transform.position, Rate);
+            transform.rotation = Quaternion.Lerp(this.transform.rotation, MainCamera.transform.rotation, Rate);
+            Rate += 0.05f;
+        }
+        if (Rate > 1 && !MainCamera.activeSelf)
+        {
+            MainCamera.SetActive(!MainCamera.activeSelf);
+            SubCamera.SetActive(!SubCamera.activeSelf);
+            Cannons.transform.Find("Cannon6Arc").gameObject.SetActive(true);
+            Cannons.transform.Find("CannonMng").gameObject.SetActive(true);
+        }
 	}
 }
