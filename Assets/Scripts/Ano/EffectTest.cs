731f7aa370692e9075c97f4b70614fe5481957d2
Anottidesu <Anottidesu@users.noreply.github.com>
2018-11-09 09:32:53 +0900

    クリティカルエフェクトと隕石サイズ調整
    クリティカルエフェクト仮作りました。
    隕石はサイズちょっとでかくしました

---
+++ Assets/Scripts/Ano/EffectTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectTest : MonoBehaviour {
+
+    public GameObject Prefab;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(Random.Range(-100, 100), 50, Random.Range(-100, 100));
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject NewObject;
+            NewObject = (GameObject)Instantiate(Prefab, transform.position, Quaternion.identity);
+            NewObject.GetComponent<Transform>().LookAt(this.transform);
+        }
+    }
+}
--- Assets/Scripts/Ano/FireBall.cs
+++ Assets/Scripts/Ano/FireBall.cs
@@ -42,5 +42,6 @@ public class FireBall : MonoBehaviour {
         {
             Destroy(this.gameObject);
         }
+        
 	}
 }
