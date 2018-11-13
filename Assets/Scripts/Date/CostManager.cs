c4d2909c2f5c50de58e550c4dc68273dbbc11f28
Date <roxas03200320@gmail.com>
2018-11-13 09:23:08 +0900

    コスト保存場所

---
+++ Assets/Scripts/Date/CostManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostManager : MonoBehaviour {
+    private Dictionary<int, float> m_playerCostData;
+    [SerializeField]
+    private float m_magnification;      // 倍率
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddCostData(int playerID,float cost)
+    {
+        m_playerCostData.Add(playerID, cost);
+    }
+
+    // コストから得点に変換し取得
+    int ConversionCost(float cost)
+    {
+        return Mathf.RoundToInt(cost * m_magnification);
+    }
+}
