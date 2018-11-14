a2c6f579245a38392d36418bc278792e49864dd1
miyahara <taru020202@gmail.com>
2018-11-14 16:48:49 +0900

    ColdSleepIceFieldUpdate11-14
    外側の壁を時間経過で消えるようにしました。
    エッジシェーダーのマテリアルを使ってるため
    アルファ値をいじってもオブジェクト自体が透明にならないがとりあえず消える処理は完成

---
+++ Assets/Scripts/Miyahara/ColdSleepGimic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColdSleepGimic : MonoBehaviour {
+
+    private GameController Gb;
+    private const int MaxWall = 6;
+    private GameObject ParentWall;
+    private VanishWall[] ChildWall;
+
+    //private int[,] WallList = 
+    //{
+    //    {0,1,2,3,4,5}               
+    //};
+    private int Count;
+
+    private float NowTime = 0;
+    [Range(0, 360)]
+    [SerializeField]
+    private float Interval;
+    [Range(0, 360)]
+    [SerializeField]
+    private float SetTime;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        if (Gb == null)
+        {
+            Gb = FindObjectOfType<GameController>();
+        }
+        Count = 0;
+        ParentWall = GameObject.Find("IceFieldAroundWalls");
+        ChildWall = ParentWall.GetComponentsInChildren<VanishWall>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Gb.StartFlg)
+        {
+            if (Count >= MaxWall)
+                return;
+            if (SetTime < NowTime)
+            {
+                ChildWall[Count].OnVanish();
+                NowTime = 0;
+                Count += 1;
+            }
+            NowTime += Interval * Time.deltaTime;
+        }
+
+	}
+}
