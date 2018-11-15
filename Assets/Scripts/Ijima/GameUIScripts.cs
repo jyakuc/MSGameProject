d0ce078b0d16a54e9b6eca19a1dfb47ec68cfa09
jyakuc <vbfbap1234@gmail.com>
2018-11-15 18:43:33 +0900

    3DオブジェクトのUI組み込みテスト
    3Dの数字をUI表示テストちゅ

--- Assets/Scripts/Ijima/GameUIScripts.cs
+++ Assets/Scripts/Ijima/GameUIScripts.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUIScripts : MonoBehaviour {
 
     public GameObject Canvas;
     public GameObject uiPlayer;
     public GameObject uiRank;
+    public RawImage[] Pannel;
+    public Texture[] Images;
+    private BattlePointGrading battlePoint;
+    private int test=0;
 
 
 
@@ -22,7 +27,25 @@ public class GameUIScripts : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-
+        //test = battlePoint.GetAllPoint();
+        int test100 = test / 100;
+        int test10 = test / 10;
+        int test1 = test % 10;
+        if (test1 == 0)
+        {
+            Pannel[0].texture = Images[test1];
+            Pannel[0].color = new Color(255, 255, 255, 1f);
+        }
+        if (test10 == 0)
+        {
+            Pannel[1].texture = Images[test10];
+            Pannel[1].color = new Color(255, 255, 255, 1f);
+        }
+        if (test100 == 0)
+        {
+            Pannel[2].texture = Images[test100];
+            Pannel[2].color = new Color(255, 255, 255, 1f);
+        }
     }
 
 }
