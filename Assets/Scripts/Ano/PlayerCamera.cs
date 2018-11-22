a030d750e2a985c4eb84115e5bce072a058b580f
Anottidesu <Anottidesu@users.noreply.github.com>
2018-11-23 00:04:28 +0900

    一旦上げ
    まだプレイヤー操作とかは入ってないけど配置とリスポーンまでは終わった

--- Assets/Scripts/Ano/PlayerCamera.cs
+++ Assets/Scripts/Ano/PlayerCamera.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour {
+public class PlayerCamera : MonoBehaviour
+{
     //public enum ZoomMode { NotZoom,ZoomIn,ZoomOut};
     //cameraのプレハブ読み込み
     public GameObject PrefabCamera;
@@ -10,6 +11,8 @@ public class PlayerCamera : MonoBehaviour {
     private GameObject CameraClone;
     //Clonecameraの情報保持
     private Camera P_Camera;
+    //プレイヤーコントローラー参照して自分が何Pか判別するよう
+    private PlayerController P_Controller;
     //ClonecameraのTransform保持
     private Transform CameraTrans;
     //注視するポイントのTransformを保持
@@ -31,6 +34,7 @@ public class PlayerCamera : MonoBehaviour {
     //Zoomしてから待機する最大時間
     public float WaitMaxTime = 1.0f;
     public bool FocusMode = false;
+    public bool StalkingMode = false;
     private GameTime gametime;
     public float test = 0;
     //一旦置いておく
@@ -41,14 +45,53 @@ public class PlayerCamera : MonoBehaviour {
         CameraClone = Instantiate(PrefabCamera, PrefabCamera.transform.position, PrefabCamera.transform.rotation);
         P_Camera = CameraClone.GetComponent<Camera>();
         CameraTrans = CameraClone.GetComponent<Transform>();
+        P_Controller = GetComponent<PlayerController>();
         SaveCameraPos = CameraTrans;
         NowSpeed = Speed;
         P_Camera.depth = 0;
         WaitNowTime = 0;
         ZoomNowTime = 0;
+        //どのキャラクターの中継か判別
+        RelayTV Relay;
+        Relay = P_Camera.GetComponent<RelayTV>();
+        Relay.SetPlayerNum(P_Controller.PlayerID);
+        //ウォーミングアップSceneの配置
+        if (StalkingMode)
+        {
+            Relay.SetWarmingMode();
+            switch (P_Controller.PlayerID)
+            {
+                case 1:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.34f, 0.68f, 0.34f, 0.34f);
+                    break;
+                case 2:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.0f, 0.68f, 0.34f, 0.34f);
+
+                    break;
+                case 3:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.0f, 0.34f, 0.34f, 0.34f);
+                    break;
+                case 4:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.68f, 0.34f, 0.34f, 0.34f);
+                    break;
+                case 5:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.68f, 0.0f, 0.34f, 0.34f);
+                    break;
+                case 6:
+                    P_Camera.depth = 1;
+                    P_Camera.rect = new Rect(0.34f, 0.0f, 0.34f, 0.34f);
+                    break;
+            }
+        }
     }
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         try
         {
             gametime = GameObject.Find("GameTime").GetComponent<GameTime>();
@@ -59,12 +102,17 @@ public class PlayerCamera : MonoBehaviour {
             gametime = null;
         }
     }
-	
-	// Update is called once per frame
-	void FixedUpdate() {
-        if (!FocusMode)
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ((!FocusMode) && (!StalkingMode))
         {
             PickupCamera();
+        }
+        else if (StalkingMode)
+        {
+            StalkingCamera();
         }
         else
         {
@@ -89,7 +137,7 @@ public class PlayerCamera : MonoBehaviour {
     //ヒットストップ用にズームするカメラ設定
     private void PickupCamera()
     {
-        if(P_Camera!=null)
+        if (P_Camera != null)
         {
             if (ZoomFlag)
             {
@@ -123,7 +171,7 @@ public class PlayerCamera : MonoBehaviour {
                 WaitNowTime = 0;
             }
         }
-        
+
 
     }
     //フォーカスを当てる時用のカメラ設定
@@ -147,13 +195,15 @@ public class PlayerCamera : MonoBehaviour {
     private void StalkingCamera()
     {
         //CameraTrans.LookAt(TargetTrans);
-        CameraTrans.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
-        var desiredPos = TargetTrans.position - TargetTrans.forward * -10 + Vector3.up* 2;
-        CameraTrans.transform.position = Vector3.Lerp(
-            SaveCameraPos.transform.position ,
-            desiredPos,
-           1
-        );
+        //CameraTrans.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        //var desiredPos = TargetTrans.position - TargetTrans.forward * -10 + Vector3.up* 2;
+        //CameraTrans.transform.position = Vector3.Lerp(
+        //    SaveCameraPos.transform.position ,
+        //    desiredPos,
+        //   1
+        //);
+        CameraTrans.position = new Vector3(TargetTrans.position.x, TargetTrans.position.y + 10, TargetTrans.position.z);
+        CameraTrans.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
     }
     public void CameraDelete()
     {
