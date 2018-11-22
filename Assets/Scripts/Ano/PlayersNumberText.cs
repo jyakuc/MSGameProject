0645a7556f22494b281c2e4c4b7470d23ae4550c
Anottidesu <Anottidesu@users.noreply.github.com>
2018-11-23 03:25:00 +0900

    HumanにHitSystem抜けてたので修正
    HumanにHitSystemが抜けていたので修正しました
    WarmingUpにサンドバッグを作るので仮配置しました

---
+++ Assets/Scripts/Ano/PlayersNumberText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayersNumberText : MonoBehaviour {
+    public RelayTV Relay;
+    private Text text;
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<Text>();
+        //プレイヤーの色にする
+        switch(Relay.GetPlayerNum())
+        {
+            case 1:
+                //赤色
+                text.color = new Color(1.0f, 0.0f, 0.0f);
+                break;
+            case 2:
+                //青色
+                text.color = new Color(0.0f, 0.0f, 1.0f);
+                break;
+            case 3:
+                //黄色
+                text.color = new Color(1.0f, 1.0f, 0.0f);
+                break;
+            case 4:
+                //緑色
+                text.color = new Color(0.0f, 1.0f, 0.0f);
+                break;
+            case 5:
+                //紫色
+                text.color = new Color(1.0f, 0.0f, 1.0f);
+                break;
+            case 6:
+                //水色
+                text.color = new Color(0.0f, 1.0f, 1.0f);
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = Relay.GetPlayerNum().ToString() + "P選手";
+
+    }
+}
---
+++ Assets/Scripts/Ano/WarmingSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmingSystem : MonoBehaviour {
+    public PlayerController[] PlayersController;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 6; i++)
+        {
+            PlayerIdleCheck(i);
+        }
+    }
+    void PlayerIdleCheck(int Num)
+    {
+        if ((PlayersController[Num].GetMyState() == PlayerController.EState.Init)|| 
+            (PlayersController[Num].GetMyState() == PlayerController.EState.Wait))
+        {
+            PlayersController[Num].PlayStart();
+        }
+    }
+}
