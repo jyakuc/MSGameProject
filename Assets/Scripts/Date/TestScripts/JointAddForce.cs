15f6f1dfa826c68643d596db150610398aa89304
Date <roxas03200320@gmail.com>
2018-11-25 00:34:02 +0900

    腕の動き改良

--- Assets/Scripts/Date/TestScripts/JointAddForce.cs
+++ Assets/Scripts/Date/TestScripts/JointAddForce.cs
@@ -6,6 +6,11 @@ public class JointAddForce : MonoBehaviour {
     public Rigidbody rigidbody;
     public Vector3 vector;
     public ForceMode forceMode;
+    public Transform axis;
+    public GameObject extendObj;
+
+    public float time;
+    private bool flg = false;
     public enum AddForceType
     {
         AddForce,
@@ -15,25 +20,56 @@ public class JointAddForce : MonoBehaviour {
     public AddForceType forceType;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 vc = axis.TransformDirection(vector);
+        Debug.Log(vc);
+        //rigidbody.
         if (Input.GetKeyDown(KeyCode.G))
         {
             switch (forceType)
             {
                 case AddForceType.AddForce:
-                    rigidbody.AddForce(vector, forceMode);
+                    rigidbody.AddForce(vc, forceMode);
                     break;
                 case AddForceType.AddRelativeForce:
-                    rigidbody.AddRelativeForce(vector, forceMode);
+                    rigidbody.AddRelativeForce(vc, forceMode);
                     break;
                 case AddForceType.AddExplosionForce:
                     //rigidbody.AddExplosionForce(vector, forceMode);
                     break;
             }
         }
-	}
+
+        if (Input.GetKey(KeyCode.H))
+        {
+            switch (forceType)
+            {
+                case AddForceType.AddForce:
+                    rigidbody.AddForce(vc, forceMode);
+                    break;
+                case AddForceType.AddRelativeForce:
+                    rigidbody.AddRelativeForce(vc, forceMode);
+                    break;
+                case AddForceType.AddExplosionForce:
+                    //rigidbody.AddExplosionForce(vector, forceMode);
+                    break;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            /*
+            if (!flg)
+            {
+                rigidbody.AddForce(vc, ForceMode.Impulse);
+                flg = true;
+            }
+            */
+            transform.position = Vector3.Slerp(transform.position, extendObj.transform.position,Time.deltaTime * time);
+           // time += Time.deltaTime;
+        }
+    }
 }
