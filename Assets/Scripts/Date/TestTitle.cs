ffc4aefd8a3abb988a5db7940d2cf65ca4bf7350
YamadaNatsuki <72ch0opper@gmail.com>
2018-11-26 09:43:01 +0900

    タイトル用テクスチャ、アニメーションの追加 TestTitleの変更 タイトル完成

--- Assets/Scripts/Date/TestTitle.cs
+++ Assets/Scripts/Date/TestTitle.cs
@@ -9,24 +9,28 @@ public class TestTitle : MonoBehaviour {
     private bool SceneChangeFlg;
     private int flame;
     private bool OneKeyFlag = false;
+    private Animator TitleAnim;
 
     private void Start(){
         SceneChangeFlg = false;
         flame = 0;
         AudioManager.GetInstance.PlayBGM(AUDIO.BGM_TITLE, AudioManager.BGM_FADE_SPEED_RATE_HIGH);
+        TitleAnim = GameObject.Find("Canvas").GetComponent<Animator>();
     }
 
     void Update(){
-        if ((Input.anyKeyDown == true)&&(!OneKeyFlag)){
+        AnimatorStateInfo state = TitleAnim.GetCurrentAnimatorStateInfo(0);
+        if ((Input.anyKeyDown == true)&&(state.IsName("PressStart") == true)){
             SceneChangeFlg = true;
-            OneKeyFlag = true;
+            //OneKeyFlag = true;
+            TitleAnim.SetBool("start", true);
             AudioManager.GetInstance.PlaySE0(AUDIO.SE_Decision);
         }
         if (SceneChangeFlg == true){
             flame++;
         }
         if (flame == SceneChangeTime){
-            SceneController.GetInstance.ChangeScene("GameScene");
+            SceneController.GetInstance.ChangeScene("GameScene",2);
         }
     }
 }
