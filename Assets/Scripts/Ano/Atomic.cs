6e0b8e99ec8c4759a3e41586bfe9f185af8bedf6
Anottidesu <Anottidesu@users.noreply.github.com>
2018-11-28 21:13:48 +0900

    冷気のエフェクトほぼ完成しました
    冷気のエフェクトほぼ完成しました
    Effectを出したいオブジェクトの子に置いて再生したいタイミングで
    IceSmokeのスプリプトを読み込んでPlayEffectを呼んでくれれば再生されます
    EndEffectを呼ぶと自然に消える様にもしてます

--- Assets/Scripts/Ano/Atomic.cs
+++ Assets/Scripts/Ano/Atomic.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Atomic : MonoBehaviour {
     ParticleSystem MyParticle;
+    //オーディオ
+    public AudioManager audio;
     // Use this for initialization
     void Awake () {
         MyParticle = GetComponent<ParticleSystem>();
-	}
+        //爆発が生成された時にSEを鳴らす
+        PlaysSe();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,4 +20,9 @@ public class Atomic : MonoBehaviour {
             Destroy(this.gameObject);
         }
 	}
+    private void PlaysSe()
+    {
+        //SEの種類弄るのはここ
+        //AudioManager.GetInstance.PlaySE0(AUDIO.SE_Hit01);
+    }
 }
