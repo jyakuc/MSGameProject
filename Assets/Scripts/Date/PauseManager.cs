cbd3e07f045ba279f486178d1e3c9cfed8c96975
Date <roxas03200320@gmail.com>
2018-11-30 01:13:29 +0900

    ポーズの黒半透明

--- Assets/Scripts/Date/PauseManager.cs
+++ Assets/Scripts/Date/PauseManager.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseManager : MonoBehaviour {
-    private bool m_pauseFlg;
-    public bool IsPause
+    private static bool gPauseFlg = false;
+    public static bool IsPause
     {
-        get { return m_pauseFlg; }
+        get { return gPauseFlg; }
     }
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static int saveID = 0;
 
-    public void OnPause(bool flg)
+    public static void OnPause(int playerID)
     {
-        if (m_pauseFlg == flg) return;
-        if (flg)
+        if (gPauseFlg && saveID != playerID) return;
+        gPauseFlg = !gPauseFlg;
+        saveID = playerID;
+        if (gPauseFlg)
         {
             // ポーズ出す
-            Debug.Log(Time.timeScale);
             Time.timeScale = 0;
         }
         else
         {
             // ポーズ外す
+            Time.timeScale = 1;
         }
     }
-    
+
+    // ポーズからタイトル
+    public static void PauseToTitle(int playerID)
+    {
+        if (playerID != saveID) return;
+        saveID = -1;
+        SceneController.GetInstance.ChangeScene("TitleScene");
+    }
 }
---
+++ Assets/Scripts/Date/PauseUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseUI : MonoBehaviour {
+    [SerializeField]
+    private Image m_mask;
+    [SerializeField]
+    private Text m_pause;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (PauseManager.IsPause)
+        {
+            m_mask.gameObject.SetActive(true);
+            m_pause.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_mask.gameObject.SetActive(false);
+            m_pause.gameObject.SetActive(false);
+        }
+	}
+}
